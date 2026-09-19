c2f3193979d2638626928451ecf4591b5faf6c54
Jie <1614094+yagay@users.noreply.github.com>
2026-09-19 17:06:34 +0100

    Bump MiniWindowGuard to 3.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yagay.MiniWindowGuard"
         minSdk = 31
         targetSdk = 37
-        versionCode = 57
-        versionName = "3.0.10"
+        versionCode = 58
+        versionName = "3.0.11"
     }
 
     buildFeatures {
