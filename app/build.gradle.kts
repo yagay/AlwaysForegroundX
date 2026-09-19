975aeb858fcbd7c5425f10c8864e1aea6bdf08d3
Jie <1614094+yagay@users.noreply.github.com>
2026-09-19 09:23:58 +0100

    Bump version to 1.6.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jieei.alwaysforeground"
         minSdk = 31
         targetSdk = 37
-        versionCode = 28
-        versionName = "1.6.10"
+        versionCode = 29
+        versionName = "1.6.11"
     }
 
     compileOptions {
