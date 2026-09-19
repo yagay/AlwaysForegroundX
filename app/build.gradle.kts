fe4f6852c037cfe70c3e027b2aa974f1e9440265
Jie <1614094+yagay@users.noreply.github.com>
2026-09-19 18:57:19 +0100

    Bump MiniWindowGuard to 4.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yagay.MiniWindowGuard"
         minSdk = 31
         targetSdk = 37
-        versionCode = 64
-        versionName = "4.3.1"
+        versionCode = 65
+        versionName = "4.4.0"
     }
 
     buildFeatures {
