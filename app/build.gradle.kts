3da0bc336c842850b4670ccc359754d939378105
Jie <1614094+yagay@users.noreply.github.com>
2026-09-19 09:25:09 +0100

    Bump version to 1.6.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jieei.alwaysforeground"
         minSdk = 31
         targetSdk = 37
-        versionCode = 29
-        versionName = "1.6.11"
+        versionCode = 30
+        versionName = "1.6.12"
     }
 
     compileOptions {
