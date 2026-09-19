b9d6c2629dd1d18bdfc5948bdabe2f43ce402f05
Jie <1614094+yagay@users.noreply.github.com>
2026-09-19 10:59:29 +0100

    Bump version to 1.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.jieei.alwaysforeground"
         minSdk = 31
         targetSdk = 37
-        versionCode = 33
-        versionName = "1.7.1"
+        versionCode = 34
+        versionName = "1.7.2"
     }
 
     compileOptions {
