a3b4b52c43bb6dc502637f98ac91767c7011d549
Jie <1614094+yagay@users.noreply.github.com>
2026-09-21 13:59:08 +0100

    Expand playback control contract

--- app/src/main/java/com/yagay/MiniWindowGuard/PlaybackControlContract.java
+++ app/src/main/java/com/yagay/MiniWindowGuard/PlaybackControlContract.java
@@ -1,10 +1,22 @@
 package com.yagay.MiniWindowGuard;
 
 final class PlaybackControlContract {
+    static final String ACTION_TOGGLE =
+            "com.yagay.MiniWindowGuard.action.TOGGLE_PLAYBACK";
+    static final String ACTION_PLAY =
+            "com.yagay.MiniWindowGuard.action.PLAY_PLAYBACK";
     static final String ACTION_PAUSE =
             "com.yagay.MiniWindowGuard.action.PAUSE_PLAYBACK";
+    static final String ACTION_PREVIOUS =
+            "com.yagay.MiniWindowGuard.action.PREVIOUS_PLAYBACK";
+    static final String ACTION_NEXT =
+            "com.yagay.MiniWindowGuard.action.NEXT_PLAYBACK";
+    static final String ACTION_DISMISS =
+            "com.yagay.MiniWindowGuard.action.DISMISS_PLAYBACK_NOTIFICATION";
+
     static final String EXTRA_PACKAGE_NAME =
             "package_name";
+
     static final String PERMISSION_CONTROL_PLAYBACK =
             "com.yagay.MiniWindowGuard.permission.CONTROL_PLAYBACK";
 
