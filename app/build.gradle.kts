9d727892ebb16396348648917256e6265d6ee1da
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 18:50:32 +0800

    Restore 5.4.5: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yagay.MiniWindowGuard"
         minSdk = 31
         targetSdk = 37
-        versionCode = 106
-        versionName = "5.5.6"
+        versionCode = 95
+        versionName = "5.4.5"
     }
 
     buildFeatures {
