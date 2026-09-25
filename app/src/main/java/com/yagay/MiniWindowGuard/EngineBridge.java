7f59f2033dbd56f5d28315fc16a7d3f640e25da6
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 18:31:53 +0800

    Bump bootstrap API to 10

--- app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
+++ app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
@@ -19,7 +19,7 @@ import dalvik.system.PathClassLoader;
  * HotReloadEngine loaded from the currently installed APK.
  */
 final class EngineBridge {
-    static final int BOOTSTRAP_API = 9;
+    static final int BOOTSTRAP_API = 10;
 
     private static final String TAG = "MiniWindowGuard";
     private static final String PACKAGE_NAME =
