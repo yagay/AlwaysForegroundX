45a96c98c62dd536198b0cfea75038bcbb644577
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 22:41:54 +0800

    Bump bootstrap API for system-window focus hook

--- app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
+++ app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
@@ -19,7 +19,7 @@ import dalvik.system.PathClassLoader;
  * HotReloadEngine loaded from the currently installed APK.
  */
 final class EngineBridge {
-    static final int BOOTSTRAP_API = 2;
+    static final int BOOTSTRAP_API = 3;
 
     private static final String TAG = "MiniWindowGuard";
     private static final String PACKAGE_NAME =
