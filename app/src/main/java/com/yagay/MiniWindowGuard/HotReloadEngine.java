96e759a9e28aba86629eb442495fead163f21e46
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 17:22:22 +0800

    Require bootstrap API 8

--- app/src/main/java/com/yagay/MiniWindowGuard/HotReloadEngine.java
+++ app/src/main/java/com/yagay/MiniWindowGuard/HotReloadEngine.java
@@ -14,7 +14,7 @@ import android.util.Log;
  */
 public final class HotReloadEngine {
     private static final String TAG = "MiniWindowGuardEngine";
-    private static final int BOOTSTRAP_API_REQUIRED = 7;
+    private static final int BOOTSTRAP_API_REQUIRED = 8;
 
     private OplusFlexibleWindowController controller;
     private volatile boolean started;
