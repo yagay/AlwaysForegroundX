035ffeb47c33a881e4795e4cd96d9b4aebe150fb
Jie <1614094+yagay@users.noreply.github.com>
2026-09-25 18:43:54 +0800

    Bump version to 5.5.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yagay.MiniWindowGuard"
         minSdk = 31
         targetSdk = 37
-        versionCode = 105
-        versionName = "5.5.5"
+        versionCode = 106
+        versionName = "5.5.6"
     }
 
     buildFeatures {
