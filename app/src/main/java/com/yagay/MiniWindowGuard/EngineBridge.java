ea2c81d0624db03df44eb51fc609afbab7098081
Jie <1614094+yagay@users.noreply.github.com>
2026-09-26 00:21:44 +0800

    Bump bootstrap API for launcher FloatHandle bridge

--- app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
+++ app/src/main/java/com/yagay/MiniWindowGuard/EngineBridge.java
@@ -19,7 +19,7 @@ import dalvik.system.PathClassLoader;
  * HotReloadEngine loaded from the currently installed APK.
  */
 final class EngineBridge {
-    static final int BOOTSTRAP_API = 4;
+    static final int BOOTSTRAP_API = 5;
 
     private static final String TAG = "MiniWindowGuard";
     private static final String PACKAGE_NAME =
